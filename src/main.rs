802bb0ac44f4e7226b5ffe606c7c89b1756cc6c1
techyangj <1066900355@qq.com>
2024-03-14 05:27:00 -0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![no_std] // 不链接 Rust 标准库
+#![no_main] // 禁用所有 Rust 层级的入口点
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // 不重整函数名
+pub extern "C" fn _start() -> ! {
+    // 因为链接器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
+    // 默认命名为 `_start`
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
